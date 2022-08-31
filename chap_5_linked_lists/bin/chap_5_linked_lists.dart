7200d08a7de230e0b0fbec4784c37e535f1f14a1
Md. Mostafizur Rahman <38970298+mostafiz9900@users.noreply.github.com>
2022-08-31 22:28:20 +0600

    link list

---
+++ chap_5_linked_lists/bin/chap_5_linked_lists.dart
@@ -0,0 +1,5 @@
+import 'package:chap_5_linked_lists/chap_5_linked_lists.dart' as chap_5_linked_lists;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chap_5_linked_lists.calculate()}!');
+}
---
+++ chap_5_linked_lists/test/chap_5_linked_lists_test.dart
@@ -0,0 +1,8 @@
+import 'package:chap_5_linked_lists/chap_5_linked_lists.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
