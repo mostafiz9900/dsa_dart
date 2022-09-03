ce15ced2336cfc20a8268392913bf4bd83a352c7
Md. Mostafizur Rahman <38970298+mostafiz9900@users.noreply.github.com>
2022-09-03 10:43:03 +0600

    Queue

---
+++ queues-6/bin/queues_6.dart
@@ -0,0 +1,5 @@
+import 'package:queues_6/queues_6.dart' as queues_6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${queues_6.calculate()}!');
+}
---
+++ queues-6/test/queues_6_test.dart
@@ -0,0 +1,8 @@
+import 'package:queues_6/queues_6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
